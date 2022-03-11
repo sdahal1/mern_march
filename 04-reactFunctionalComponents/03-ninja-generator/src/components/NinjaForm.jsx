9461cb95a4e589e8fb20354043f699123d5520f5
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-03-11 14:14:10 -0800

    delete and update ninja using extra callback parameters

--- 04-reactFunctionalComponents/03-ninja-generator/src/components/NinjaForm.jsx
+++ 04-reactFunctionalComponents/03-ninja-generator/src/components/NinjaForm.jsx
@@ -9,6 +9,9 @@ const NinjaForm = ()=>{
     let [favColor, setFavColor] = useState("");
     let [favLang, setFavLang] = useState("");
 
+    //variable for if student has completed a project or if they have not--> similar to to-do list assignment where a task is complete or it is not
+    let [projectUploaded, setProjectUploaded] = useState(false)
+
     //state variable to store all the submitted ninja objects inside and initialize it as an empty list
     let [ninjaList, setNinjaList] = useState([])
 
@@ -16,7 +19,7 @@ const NinjaForm = ()=>{
     const submitHandler = (e)=>{
         e.preventDefault(); //prevents the form from reloading the whole page
         console.log({name, profilePicUrl, favColor, favLang})
-        let newNinjaObj = {name, profilePicUrl, favColor, favLang} //create a ninja object using the info from form
+        let newNinjaObj = {name, profilePicUrl, favColor, favLang, projectUploaded} //create a ninja object using the info from form
 
         //clear out the state variables which will clear out the form inputs if i assign the from inputs values to be the state variables
         setName("")
@@ -28,6 +31,41 @@ const NinjaForm = ()=>{
         // ninjaList.push(newNinjaObj) //this happened to work, but is not best practice in react. We must use the setter to set state when we want to update state
 
         setNinjaList([...ninjaList, newNinjaObj]) //set the ninjaList variable using the setter (setNinjaList()) to contain a copy of whatever is currently already in the ninja list (...ninjaList) and add the new ninja object to the end of that list (, newNinjaObj);
+
+    }
+
+
+    //delete ninja function
+    const deleteNinja = (i)=>{
+        console.log("deleting at index -->", i) //i represents the index of the ninja we clicked the delete button
+
+        //use the filter function to loop through the original array and filter out the item we want to delete by the index number 
+        let filteredNinjaList = ninjaList.filter((ninja,index)=>{
+            return index!=i;
+        })
+
+        console.log("filtered ninja list --->", filteredNinjaList)
+
+        //update our state array ninjaList to be this new filteredNinjaList
+        setNinjaList(filteredNinjaList)
+    }
+
+    const updateStudent = (i)=>{
+        console.log("index number of student we want to update is -->", i)
+        console.log("here is the ninja want to update", ninjaList[i].projectUploaded)
+
+        //make a copy of our original array and modify the copy
+        let copyOfNinjaList = [...ninjaList]
+        //update the projectUploaded project of the ninja object from the ninjasList at the specified index to toggle from false to true or true to false
+        copyOfNinjaList[i].projectUploaded = !copyOfNinjaList[i].projectUploaded
+
+
+        //update the state variable using the modified copy so that react knows to update the DOM
+        setNinjaList(copyOfNinjaList)
+
+
+
+
 
     }
 
@@ -62,12 +100,15 @@ const NinjaForm = ()=>{
             </form>
    
             {
-              ninjaList.map(ninja=>{
-                  return <div style= {{backgroundColor: ninja.favColor}}>
-                      <h1>{ninja.name}</h1>
+              ninjaList.map((ninja,i)=>{
+                  return <div key={i} style= {{backgroundColor: ninja.favColor, border: ninja.projectUploaded? "20px solid gold" : ""}}>
+                      <h1>{ninja.name} - Index number: {i}</h1>
                       <p>Favorite Color: {ninja.favColor}</p>
                       <p>Favorite Language: {ninja.favLang}</p>
                       <p><img src={ninja.profilePicUrl} alt="" height="200px" width="200px" /></p>
+                      <button onClick={()=>deleteNinja(i)} className="btn btn-danger">Delete {ninja.name}</button>
+                      <label htmlFor="">Student completed project</label>
+                      <input onClick = {()=>updateStudent(i)} type="checkbox" name="" id="" />
                       <hr />
                   </div>
               }) 
